caf250208cc402e92046e2564ec7c5ac02e7c5c3
deemanth <deemanth.poonacha@ikara.ai>
2024-10-21 17:16:06 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col h-svh row-start-2 items-center sm:items-start">
+      <header>header</header>
+      <div className=""></div>
+      <footer className="mt-auto">footer</footer>
+    </main>
+  );
+}
