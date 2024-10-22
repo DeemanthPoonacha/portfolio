f2d9edb3b0a5450b2518df439962ec784c1b44d7
deemanth <deemanth.poonacha@ikara.ai>
2024-10-22 14:18:30 +0530

    Computer 3d model added

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-white dark:bg-gray-900 shadow-md">
       <div className="container mx-auto px-4 py-6 text-center">
         <p className="text-gray-600 dark:text-gray-400">
-          &copy; 2024 John Doe. All rights reserved.
+          &copy; 2024 Deemanth Poonacha. All rights reserved.
         </p>
         <div className="mt-2 flex justify-center space-x-4">
           <a
--- components/hero.tsx
+++ components/hero.tsx
@@ -3,32 +3,40 @@
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
 import FloatingLogos from "./floating-logo";
+import ComputersCanvas from "./canvas/computer";
 
 export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center h-screen lg:h-[70vh] relative py-20 bg-gradient-to-r from-gray-50 to-gray-100 dark:from-gray-950 dark:to-gray-900 overflow-hidden">
-      <div className="container px-4 text-center relative z-10">
+      <div className="container px-4 text-center relative z-10 flex justify-between">
+        <ComputersCanvas />
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
+          className="flex flex-col items-start"
         >
-          <h1 className="text-5xl font-bold mb-4 text-gray-800 dark:text-gray-100">
+          <h1 className="text-5xl font-bold text-left mb-4 text-gray-800 dark:text-gray-100">
             Deemanth Poonacha
           </h1>
-          <h2 className="text-2xl mb-8 text-gray-600 dark:text-gray-300">
+          <h2 className="text-2xl text-left mb-8 text-gray-600 dark:text-gray-300">
             Full Stack Developer
           </h2>
-          <p className="text-lg mb-8 max-w-2xl mx-auto text-gray-600 dark:text-gray-300">
+          <p className="text-lg text-left mb-8 max-w-2xl mx-auto text-gray-600 dark:text-gray-300">
             Passionate about creating robust and scalable web applications.
+            <br />
             Experienced in both front-end and back-end technologies.
           </p>
-          <Button asChild className="bg-primary text-white hover:bg-primary/90">
-            <a href="#contact">Contact Me</a>
-          </Button>
+
+          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 shadow-2xl">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              <a href="#contact">Contact Me</a>
+            </span>
+          </button>
         </motion.div>
       </div>
-      <FloatingLogos />
+      {/* <FloatingLogos /> */}
     </section>
   );
 }
