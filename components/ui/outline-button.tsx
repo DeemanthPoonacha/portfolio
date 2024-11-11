a3cdbf2488ab5333300bc6dbc0cd555a5311cede
deemanth <deemanth.poonacha@ikara.ai>
2024-11-11 14:11:01 +0530

    Disabled state added for the button

--- components/ui/outline-button.tsx
+++ components/ui/outline-button.tsx
@@ -7,6 +7,7 @@ export interface ButtonProps {
   spanClassName?: string;
   children?: React.ReactNode;
   type?: "submit" | "reset" | "button" | undefined;
+  disabled?: boolean;
 }
 const Button = (props: ButtonProps) => {
   const fadeInUpVariants = {
@@ -23,18 +24,28 @@ const Button = (props: ButtonProps) => {
   return (
     <motion.button
       variants={fadeInUpVariants}
-      whileHover={{ y: 2 }}
-      whileTap={{ scale: 0.95 }}
+      whileHover={props.disabled ? {} : { y: 2 }}
+      whileTap={props.disabled ? {} : { scale: 0.95 }}
+      disabled={props.disabled}
       className={cn(
-        "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 group relative h-12 overflow-hidden p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 shadow-2xl",
-        props.className
+        "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed  disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 group relative h-12 overflow-hidden p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 shadow-2xl",
+        props.className,
+        props.disabled ? "cursor-not-allowed" : ""
       )}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#2563EB_0%,#9333EA_50%,#2563EB_100%)] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#60a5fa_0%,#c084fc_50%,#60a5fa_100%)]" />
       <span
         className={cn(
-          "inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-slate-950 px-6 py-1 text-sm font-medium text-white backdrop-blur-3xl transition-colors duration-200 group-hover:bg-slate-900/30",
-          props.spanClassName
+          "absolute",
+          props.disabled
+            ? "bg-gray-50"
+            : "inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#2563EB_0%,#9333EA_50%,#2563EB_100%)] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#60a5fa_0%,#c084fc_50%,#60a5fa_100%)]"
+        )}
+      />
+      <span
+        className={cn(
+          "inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-slate-950 px-6 py-1 text-sm font-medium text-white backdrop-blur-3xl transition-colors duration-200",
+          props.spanClassName,
+          props.disabled ? "cursor-not-allowed" : "group-hover:bg-slate-900/30"
         )}
       >
         {props.children}
